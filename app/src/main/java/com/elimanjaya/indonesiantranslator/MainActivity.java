bdef2ad2889c803940c72cb378fae00e9fb11e93
evelynlimanjaya <crimson.eve@gmail.com>
2023-09-08 16:06:53 -0700

    Adds voice playback to the buttons

--- app/src/main/java/com/elimanjaya/indonesiantranslator/MainActivity.java
+++ app/src/main/java/com/elimanjaya/indonesiantranslator/MainActivity.java
@@ -2,7 +2,9 @@ package com.elimanjaya.indonesiantranslator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +20,55 @@ public class MainActivity extends AppCompatActivity {
         eveningBtn = findViewById(R.id.goodEveningBtn);
         howBtn = findViewById(R.id.howAreYouBtn);
         byeBtn = findViewById(R.id.goodByeBtn);
+
+        morningBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.pagi);
+
+                mp.start();
+
+            }
+        });
+
+        afternoonBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.siang);
+
+                mp.start();
+
+            }
+        });
+
+        eveningBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.malam);
+
+                mp.start();
+
+            }
+        });
+
+        howBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.kabar);
+
+                mp.start();
+
+            }
+        });
+
+        byeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.tinggal);
+
+                mp.start();
+
+            }
+        });
     }
 }
